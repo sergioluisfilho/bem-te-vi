b2e6f2c652dbc57f08804becf4299a4d37c4ee0b
sergioluisfilho <sergioluisfilho@hotmail.com>
2021-08-17 20:47:22 -0300

    btv

---
+++ index.js
@@ -0,0 +1,24 @@
+const {Intents, Client} = require('discord.js');
+const token = 'ODc3MzIxNDQ5MjU2NDExMTk2.YRw7RQ.yDuIWEqA9snC5FFtCG7xXMrFfuA'; //process.env.TOKEN;
+
+const prefix = '!';
+
+const app = new Client();
+
+
+app.on('ready', () => {
+    console.log('Bem-te-vi');
+});
+
+
+app.on('message', async (message) => {
+    if (message.content === `${prefix}btv`){
+            message.member.voice.channel.join().then(connection => {
+            const dispatcher = connection.play('btv.mp3');
+            message.reply('bem-te-vi')
+            dispatcher.on('finish', () => { message.member.voice.channel.leave() });
+        });
+    } 
+});
+
+app.login(token);
